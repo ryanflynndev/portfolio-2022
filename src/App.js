6d3c440f98cfa13ff63d5af98bb2d27abcc86e0b
Ryan Flynn <ryanflynn44@gmail.com>
2022-01-26 21:43:09 -0500

    Added cool animation

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import Navbar from "./components/Navbar";
 import { Canvas } from '@react-three/fiber';
 import Particles  from './components/Particles';
 import Languages from "./components/Languages";
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [title, setTitle] = useState(0);
+
+  const titles = ['Full-Stack', 'Frontend', 'Backend', 'Blockchain'];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTitle(title => title + 1);
+      
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if(title > 3) {
+    setTitle(title => title = 0);
+  }
+
   return (
   <div className="h-screen w-screen bg-black overflow-auto">
     <Navbar/>
-    <div className="pt-32 text-center">
+    <div className="pt-32 text-center container mx-auto relative">
       <h2 className="font-extrabold text-7xl text-white font-inter py-20" >
         Hi I'm Ryan Flynn and I'm a
       </h2>
       <h2 className="font-extrabold text-7xl text-white font-inter bg-gradient-to-r bg-clip-text text-transparent
-from-blue-400 
-to-orange-500 
-via-purple-500 animate-gradient-x pb-1.5" >
-        Full-Stack Engineer
+        from-blue-400 
+        to-orange-500 
+        via-purple-500 animate-gradient-x pb-1.5" >
+        {titles[title]} Engineer
       </h2>
     </div>
     {/* <Canvas>
